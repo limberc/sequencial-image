dc5ad6b5e31d7d0d48a3260cad5650d8553d3a77
程泽华 <chengzehua@outlook.com>
2020-12-03 17:06:08 +0800

    :tada: Init commit

---
+++ main.py
@@ -0,0 +1,239 @@
+from argparse import ArgumentParser, Namespace
+
+import pytorch_lightning as pl
+import torch
+import torch.nn.parallel
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+from pytorch_lightning.core import LightningModule
+from torch import nn
+from torchvision.datasets import CIFAR10, CIFAR100
+
+from models import TransformerModel
+
+
+def get_dataset(data_path, dataset):
+    if dataset == 'cifar10':
+        mean = [x / 255 for x in [125.3, 123.0, 113.9]]
+        std = [x / 255 for x in [63.0, 62.1, 66.7]]
+    elif dataset == 'cifar100':
+        mean = [x / 255 for x in [129.3, 124.1, 112.4]]
+        std = [x / 255 for x in [68.2, 65.4, 70.4]]
+    train_transform = transforms.Compose(
+        [transforms.RandomHorizontalFlip(),
+         # transforms.RandomCrop(32, padding=4),
+         transforms.ToTensor(),
+         transforms.Normalize(mean, std)])
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(mean, std)])
+    if dataset == 'cifar10':
+        train_data = CIFAR10(data_path, train=True, transform=train_transform, download=True)
+        test_data = CIFAR10(data_path, train=False, transform=test_transform, download=True)
+    elif dataset == 'cifar100':
+        train_data = CIFAR100(data_path, train=True, transform=train_transform, download=True)
+        test_data = CIFAR100(data_path, train=False, transform=test_transform, download=True)
+    return train_data, test_data
+
+
+class CIFARLightning(LightningModule):
+    # pull out resnet names from torchvision models
+    def __init__(
+            self,
+            arch: str,
+            lr: float,
+            momentum: float,
+            weight_decay: int,
+            data_path: str,
+            dataset: str,
+            batch_size: int,
+            workers: int,
+            nhead: int,
+            nhid: int,
+            nlayer: int,
+            dropout: int,
+            **kwargs,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.arch = arch
+        # self.pretrained = pretrained
+        self.lr = lr
+        self.momentum = momentum
+        self.weight_decay = weight_decay
+        self.data_path = data_path
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.workers = workers
+        self.model = TransformerModel(ntoken=32 * 32, ninp=3, nhead=nhead, nhid=nhid, nlayers=nlayer,
+                                      dropout=dropout)
+        self.criterion = nn.NLLLoss()
+        self.train_datset, self.test_dataset = get_dataset(data_path, dataset)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        data, target = batch
+        data = data.view(-1, 3, 32 * 32)
+        output = self(data)
+        loss_val = self.criterion(output, target)
+        acc1, acc5 = self.__accuracy(output, target, topk=(1, 5))
+        self.log('train_loss', loss_val, on_step=True, on_epoch=True, logger=True)
+        self.log('train_acc1', acc1, on_step=True, prog_bar=True, on_epoch=True, logger=True)
+        self.log('train_acc5', acc5, on_step=True, on_epoch=True, logger=True)
+        return loss_val
+
+    def validation_step(self, batch, batch_idx):
+        images, target = batch
+        output = self(images)
+        loss_val = self.criterion(output, target)
+        acc1, acc5 = self.__accuracy(output, target, topk=(1, 5))
+        self.log('val_loss', loss_val, on_step=True, on_epoch=True)
+        self.log('val_acc1', acc1, on_step=True, prog_bar=True, on_epoch=True)
+        self.log('val_acc5', acc5, on_step=True, on_epoch=True)
+
+    @staticmethod
+    def __accuracy(output, target, topk=(1,)):
+        """Computes the accuracy over the k top predictions for the specified values of k"""
+        with torch.no_grad():
+            maxk = max(topk)
+            batch_size = target.size(0)
+
+            _, pred = output.topk(maxk, 1, True, True)
+            pred = pred.t()
+            correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+            res = []
+            for k in topk:
+                correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+                res.append(correct_k.mul_(100.0 / batch_size))
+            return res
+
+    def configure_optimizers(self):
+        optimizer = optim.SGD(
+            self.parameters(),
+            lr=(self.lr or self.learning_rate),
+            momentum=self.momentum,
+            weight_decay=self.weight_decay,
+            nesterov=True
+        )
+        return [optimizer]
+
+    def train_dataloader(self):
+        train_loader = torch.utils.data.DataLoader(
+            dataset=self.train_datset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.workers,
+            pin_memory=True,
+            drop_last=True
+        )
+        return train_loader
+
+    def val_dataloader(self):
+        val_loader = torch.utils.data.DataLoader(
+            dataset=self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.workers,
+            pin_memory=True,
+            drop_last=True
+        )
+        return val_loader
+
+    def test_dataloader(self):
+        test_loader = torch.utils.data.DataLoader(
+            dataset=self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.workers,
+            pin_memory=True,
+            drop_last=True
+        )
+        return test_loader
+
+    def test_step(self, *args, **kwargs):
+        return self.validation_step(*args, **kwargs)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):  # pragma: no-cover
+        parser = ArgumentParser(parents=[parent_parser])
+        parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                            choices=CIFARLightning.MODEL_NAMES,
+                            help=('model architecture: ' + ' | '.join(CIFARLightning.MODEL_NAMES)
+                                  + ' (default: resnet18)'))
+        parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                            help='number of data loading workers (default: 4)')
+        parser.add_argument('--nhead', default=4, type=int, metavar='N',
+                            help='number of head in Transformer')
+        parser.add_argument('--nhid', default=800, type=int, metavar='N',
+                            help='number of hidden layer in Transformer')
+        parser.add_argument('--nlayer', default=4, type=int, metavar='N',
+                            help='number of layer in Transformer')
+        parser.add_argument('--dropout', default=0.5, type=float, metavar='N',
+                            help='dropout rate in Transformer')
+        parser.add_argument('-b', '--batch-size', default=512, type=int,
+                            metavar='N',
+                            help='mini-batch size (default: 256), this is the total '
+                                 'batch size of all GPUs on the current node when '
+                                 'using Data Parallel or Distributed Data Parallel')
+        parser.add_argument('-lr', '--learning-rate', default=0.4, type=float,
+                            metavar='LR', help='initial learning rate', dest='lr')
+
+        parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                            help='momentum')
+        parser.add_argument('--wd', '--weight-decay', default=5e-4, type=float,
+                            metavar='W', help='weight decay (default: 5e-4)',
+                            dest='weight_decay')
+        parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                            help='use layer0-trained model')
+
+        return parser
+
+
+def main(args: Namespace) -> None:
+    if args.seed is not None:
+        pl.seed_everything(args.seed)
+
+    if args.distributed_backend == 'ddp':
+        # When using a single GPU per process and per
+        # DistributedDataParallel, we need to divide the batch size
+        # ourselves based on the total number of GPUs we have
+        args.batch_size = int(args.batch_size / max(1, args.gpus))
+        args.workers = int(args.workers / max(1, args.gpus))
+
+    model = CIFARLightning(**vars(args))
+    trainer = pl.Trainer.from_argparse_args(args)
+    if args.auto_lr_find or args.auto_scale_batch_size:
+        trainer.tune(model)
+
+    if args.evaluate:
+        trainer.test(model)
+    else:
+        trainer.fit(model)
+
+
+def run_cli():
+    parent_parser = ArgumentParser(add_help=False)
+    parent_parser = pl.Trainer.add_argparse_args(parent_parser)
+    parent_parser.add_argument('--data-path', metavar='DIR', type=str, default='./data',
+                               help='path to dataset')
+    parent_parser.add_argument('--dataset', type=str, default='cifar10',
+                               help='CIFAR10 or CIFAR100')
+    parent_parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                               help='evaluate model on validation set')
+    parent_parser.add_argument('--seed', type=int, default=42,
+                               help='seed for initializing training.')
+    parser = CIFARLightning.add_model_specific_args(parent_parser)
+    parser.set_defaults(
+        profiler=True,
+        deterministic=True,
+        max_epochs=200,
+    )
+    args = parser.parse_args()
+    main(args)
+
+
+if __name__ == '__main__':
+    run_cli()
